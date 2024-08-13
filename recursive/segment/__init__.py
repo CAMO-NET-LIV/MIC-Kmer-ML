62bedeffc01d5cadf56ffeeec0904ef07b3d9352
Yinzheng (William) Zhong <yinzheng.zhong@liverpool.ac.uk>
2024-08-13 09:49:32 +0100

    Add SegmentManager, Extender, and config module
    This commit introduces the SegmentManager and Extender classes to manage and extend genomic segments. Additionally, it includes configuration settings required by the SegmentManager. These changes integrate segment extension and management functionalities.

---
+++ recursive/segment/__init__.py
@@ -0,0 +1,3 @@
+from recursive.segment.segment_manager import SegmentManager as _SegmentManager
+
+seg_manager = _SegmentManager()
---
+++ recursive/segment/extender.py
@@ -0,0 +1,41 @@
+import itertools
+from recursive.segment import seg_manager
+from recursive.log import logger
+
+
+class Extender:
+    def __init__(
+            self,
+            keep_read_error: bool = False
+    ):
+        self.keep_read_error = keep_read_error
+        self.nucleotides = ['a', 't', 'g', 'c']
+        if self.keep_read_error:
+            self.nucleotides.append('n')
+
+    def _extend_one_seg(self, sequence: str, length: int):
+        # generate all possible extensions
+        extensions = [''.join(p) for p in itertools.product(self.nucleotides, repeat=length)]
+
+        extended_sequences = [extension[:i] + sequence + extension[i:]
+                              for extension in extensions
+                              for i in range(len(extension) + 1)]
+
+        return extended_sequences
+
+    def extend_all_segs(self, length: int):
+        new = [self._extend_one_seg(sequence, length) for sequence in seg_manager]
+        # reshape the list of lists to a single list
+        new = [item for sublist in new for item in sublist]
+
+        logger.info(f'Adding {len(new)} new subsequences to lookup')
+
+        seg_manager.add_subsequences(new)
+
+
+if __name__ == '__main__':
+    extender = Extender()
+    o = extender._extend_one_seg('aaaaaaaa', 4)
+    print(seg_manager)
+    print(len(seg_manager))
+    print(seg_manager[0])
